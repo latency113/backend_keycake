e10cb78ce629ca12d868695a36281df284ba243e
porramet <porkrubpom@gmail.com>
2025-08-25 14:49:24 +0700

    :bug: fix(api): fix

--- src/controllers/Classroom/get-all.ts
+++ src/controllers/Classroom/get-all.ts
@@ -5,7 +5,7 @@ import DatabaseContext from "@/repositories/prisma.js"
 import { ClassroomService } from "@/services/index.js"
 import { BaseRequestQuerySchema } from "@/types/global/index.js"
 import { FailResponseSchema } from "@/types/global/response.js"
-import { ClassroomWithDepartmentSchema } from "@/types/schema/prisma/index"
+import { ClassroomWithDepartmentSchema } from "@/types/schema/prisma"
 
 const ResponseSchema = z.object({
   data: ClassroomWithDepartmentSchema.array(),
